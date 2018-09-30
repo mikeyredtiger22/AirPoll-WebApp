1a283414e204cc8f0dfb9e3f180086cd88b4658f
Unknown <mikeyredtiger22@gmail.com>
2018-09-30 20:35:49 +0100

    Added date and time range filter slider.

--- Filter.js
+++ Filter.js
@@ -7,23 +7,42 @@ function createTreatmentFilters(treatments) {
 }
 
 export function addSliders() {
-  let dateSlider = document.getElementById('dateSlider');
-  let timeSlider = document.getElementById('timeSlider');
+  let dateSliderElement = document.getElementById('dateFilterSlider');
+  let timeSliderElement = document.getElementById('timeFilterSlider');
 
-  noUiSlider.create(dateSlider, {
-    start: [20, 80],
+  let dateSlider = noUiSlider.create(dateSliderElement, {
+    start: [new Date('2018').getTime(), new Date('2019').getTime()],
     connect: true,
+    behaviour: 'drag-snap',
+    step: (24 * 60 * 60 * 1000),
     range: {
-      'min': 0,
-      'max': 100
+      'min': new Date('2018').getTime(),
+      'max': new Date('2019').getTime(),
     }
   });
-  noUiSlider.create(timeSlider, {
-    start: [20, 80],
+  let timeSlider = noUiSlider.create(timeSliderElement, {
+    start: [0, (24 * 60 * 60 * 1000)],
     connect: true,
+    behaviour: 'drag-snap',
+    step: (60 * 60 * 1000),
     range: {
       'min': 0,
-      'max': 100
+      'max': (24 * 60 * 60 * 1000)
     }
   });
+
+  dateSlider.on('change', function (values, handle, unencoded) {
+    let startDateString = new Date(unencoded[0]).toDateString();
+    let endDateString = new Date(unencoded[1]).toDateString();
+    let dateRangeString = startDateString + ' - ' + endDateString;
+    document.getElementById('dateRangeOutput').innerHTML = 'Date Range: ' + dateRangeString;
+  });
+
+  timeSlider.on('change', function (values, handle, unencoded) {
+    let startTimeString = parseInt(unencoded[0]/(60 * 60 * 1000));
+    let endTimeString = parseInt(unencoded[1]/(60 * 60 * 1000)); //todo fix 15.000...2
+    let timeRangeString = startTimeString + ' - ' + endTimeString;
+    document.getElementById('timeRangeOutput').innerHTML = 'Time Range: ' + timeRangeString;
+  });
+
 }
