431358b67af7dc5c9aa4912f0274ed715a10ca5d
Unknown <mikeyredtiger22@gmail.com>
2018-10-21 19:02:23 +0100

    Added environment variables (dotenv). Changed heroku node version.

---
+++ FirebaseCredentials.js
@@ -0,0 +1,10 @@
+export function firebaseCredentials() {
+  return {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+  };
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './AirPollMain.js',
@@ -24,6 +25,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin('build'),
+    new Dotenv(),
     new MiniCssExtractPlugin({
       filename: 'nouislider.css',
     }),
