8c0d39b21b3e7a30e1516a457cff10ffcce0e803
Unknown <mikeyredtiger22@gmail.com>
2018-10-21 21:21:55 +0100

    Changing environment variable initialisation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './AirPollMain.js',
@@ -25,7 +26,16 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin('build'),
-    new Dotenv(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'apiKey': JSON.stringify(process.env.apiKey),
+        'authDomain': JSON.stringify(process.env.authDomain),
+        'databaseURL': JSON.stringify(process.env.databaseURL),
+        'projectId': JSON.stringify(process.env.projectId),
+        'storageBucket': JSON.stringify(process.env.storageBucket),
+        'messagingSenderId': JSON.stringify(process.env.messagingSenderId),
+      }
+    }),
     new MiniCssExtractPlugin({
       filename: 'nouislider.css',
     }),
