8bbc47ed00a1327b010f38e545c6625293d67782
Michael P-F <mikeyredtiger22@gmail.com>
2018-12-29 17:04:01 +0000

    Changed hosting to firebase. Using dotenv-webpack. Removed serve.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
 const path = require('path');
-const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -39,16 +38,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin('build'),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'apiKey': JSON.stringify(process.env.apiKey),
-        'authDomain': JSON.stringify(process.env.authDomain),
-        'databaseURL': JSON.stringify(process.env.databaseURL),
-        'projectId': JSON.stringify(process.env.projectId),
-        'storageBucket': JSON.stringify(process.env.storageBucket),
-        'messagingSenderId': JSON.stringify(process.env.messagingSenderId),
-      }
-    }),
+    new Dotenv(),
     new MiniCssExtractPlugin({
       filename: 'nouislider.css',
     }),
